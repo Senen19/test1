a1b378b8d41ad70ce513aadc3b6ff5574a63b808
Senen1997 <71589700+Senen1997@users.noreply.github.com>
2020-10-06 11:38:35 +0200

    Ejercicio3 Terminado
    Ejercicio3 comentado bien y terminado. Va para examen.

--- TEST01/TEST01/Utils.cs
+++ TEST01/TEST01/Utils.cs
@@ -37,29 +37,30 @@ namespace TEST01
             int i = 2;                          //iniciamos el contador en 2 porque si lo iniciamos en 0 cuando
                                                 //llegue a 1 dirá que ya es primo.
 
-            while (i < number)                  //Mientras que el contador(i) sea más pequeño que el número que 
-                                                //queramos saber si es primo o no...
+            while (i < number)                  //While se ejecutará mientras que el contador(i) sea más pequeño que el número que 
+                                                //queramos saber si es primo o no('number')...
             {
-                if ((number % i) == 0)          //Para que se cumpla la cond. el número que elijamos entre contador, su resto
-                                                // deberá ser igual a 0, en ese caso será primo.
-                    return false;               // si esto no ocurre dará false, es decir, no es primo. El contador se incrementará en 1
-                                                // hasta que deje de cumplirse la condición del while para comprobar así todos
-                                                //los valores posibles en la variable number.
+                if ((number % i) == 0)          //Para que se cumpla la condición, el número ('number') que elijamos entre contador, su resto
+                                                //deberá ser igual a 0, en ese caso será primo.
+                    return false;               //Si el if no se cumple nos dirijirá a 'return false;', es decir, no es primo. El contador se incrementará en 1
+                                                //mientras el if se cumpla hasta que deje de cumplirse la condición del while para comprobar así todos
+                                                //los valores posibles en la variable number. En el caso de que la condición if NO se cumpla,
+                                                //lo que devolverá es 'return false' y terminará la función, ya que al recibir un return las funciones que
+                                                //son diferentes a void terminan. En este caso, si el if se cumple recibirá un true y seguirá el bucle
+                                                //hasta agotar la condición del while o no se cumpla la condición del if.
+                                                //En esta función de tipo bool, un 'false' es admitido en el return, con lo que al recibir el return una vez
+                                                //cumplida la condición terminará la función y entonces, al no cumplirse el IF, sabremos que el valor de 'number'
+                                                //no es un número primo ya que su resto será diferente de 0.
 
                 i++;                          
             }
-            return true;                        //si se cumple la condición del if, dará un true. Cuando la condición del while deje
-                                                //de cumplirse saldrá del bucle y nos encontraremos con tantos 'true' como 'false'
-                                                //se haya incrementado el contador 'i' hasta llegar al número que hayamos introducido
-                                                //en la variable 'number'. En caso de que alguna vez dentro del bucle se haya cumplido
-                                                //la condición del if, nos habrá devuelto un valor 'true'. Por tanto, como hemos
-                                                //creado una función que debe devolver o 'true' o 'false' al ser bool, indicaremos que
-                                                //solo nos devuelva los valores true que hemos obtenido del bucle tras comprobar
-                                                //'number' de veces con el condicional if: si es primo('false') -un primo es un valor
-                                                //que dividido entre otro número no debe dar de resido 0- o no('true'). Si 
-                                                //indicamos que nos devuelva true, solo nos lo devolverá si la variable 'number' es
-                                                //primo -es decir, la condición if se ha cumplido ('true') y el número introducido es primo-.
-
+            return true;                        //si se cumple la condición del if, dará un true y el bucle seguirá omitiendo el 'return false', 
+                                                //con lo que el contador seguirá aumentando hasta que deje de cumplirse la condición de while.
+                                                //Cuando la condición del while deje
+                                                //de cumplirse saldrá del bucle y la función nos dirijirá a 'return true', es decir 'number' es un número primo.
+                                                //porque se ha cumplido la condición del if y se ha agotado la condición del while. En caso de que alguna vez dentro del bucle NO se haya cumplido
+                                                //la condición del if, nos remitirá a 'return false'. Por tanto, la función estará terminada.
+                                               
         }
     }
 }
